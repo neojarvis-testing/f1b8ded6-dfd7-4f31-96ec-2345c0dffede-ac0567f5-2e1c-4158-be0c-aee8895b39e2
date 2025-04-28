d957aa74e0b3629f282f035677299a081f5ce447
Gowtham26003 <pgowtham26003@gmail.com>
2025-04-28 09:27:37 +0000

    added blogpost newlly

--- angularapp/src/app/services/blog-post.service.ts
+++ angularapp/src/app/services/blog-post.service.ts
@@ -9,5 +9,34 @@ import { environment } from 'src/environments/environment';
 })
 export class BlogPostService {
 
-  constructor() { }
+  private baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+  }
+
+  getAllBlogPosts(): Observable<BlogPost[]> {
+    return this.http.get<BlogPost[]>(`${this.baseUrl}/api/blogposts`, { headers: this.getAuthHeaders() });
+  }
+
+  getBlogPostById(id: number): Observable<BlogPost> {
+    return this.http.get<BlogPost>(`${this.baseUrl}/api/blogposts/${id}`, { headers: this.getAuthHeaders()});
+  }
+
+  addBlogPost(blogPost: BlogPost): Observable<any> {
+    return this.http.post(`${this.baseUrl}/api/blogposts`, blogPost, { headers: this.getAuthHeaders(), responseType: 'text' as 'json' });
+  }
+
+  updateBlogPost(id: number, blogPost: BlogPost): Observable<any> {
+    return this.http.put(`${this.baseUrl}/api/blogposts/${id}`, blogPost, { headers: this.getAuthHeaders(), responseType: 'text' as 'json' });
+  }
+
+  deleteBlogPost(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/api/blogposts/${id}`, { headers: this.getAuthHeaders(), responseType: 'text' as 'json' });
+  }
 }
