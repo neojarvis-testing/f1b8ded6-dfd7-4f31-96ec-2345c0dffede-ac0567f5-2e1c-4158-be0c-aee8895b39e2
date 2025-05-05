07c2dbea78ad5a5bfe6e6ccd7a3183bed5345935
Yakshith-12 <ysyakshithsai@gmail.com>
2025-05-05 14:24:14 +0000

    component commit

--- dotnetapp/Services/ChatHub.cs
+++ dotnetapp/Services/ChatHub.cs
@@ -7,7 +7,7 @@ namespace dotnetapp.Services  // namespace needed
         // This method can be invoked by connected clients to send a message.
         public async Task SendMessage(string user, string message)
         {
-            // Broadcast the message to all connected clients.
+            // Broadcast the message to all connected client
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
     }
