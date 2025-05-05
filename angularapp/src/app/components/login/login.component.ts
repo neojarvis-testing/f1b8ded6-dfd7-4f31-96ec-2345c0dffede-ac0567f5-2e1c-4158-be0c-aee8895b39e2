55b9eec47c4593c514e3d3c8482738abaf62d7af
Yakshith-12 <ysyakshithsai@gmail.com>
2025-05-05 14:26:28 +0000

    changes commit

--- angularapp/src/app/components/login/login.component.ts
+++ angularapp/src/app/components/login/login.component.ts
@@ -60,7 +60,7 @@ export class LoginComponent implements OnInit {
     );
   }
 
-  // Called when the OTP verification form is submitted.
+  // Called when the OTP verification form is Submitted.
   verifyOtp(): void {
     if (!this.user.Email) {
       this.loginError = 'Email is required for OTP verification';
